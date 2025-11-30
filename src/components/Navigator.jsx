9619b8378831d20702ddbdcc1464473b1f553522
Murad Ahmedli <ahmedli.murad06@bk.ru>
2025-11-30 16:41:43 +0400

    version2

---
+++ src/components/Navigator.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import Products from "../pages/Products"
+import ProductDetails from "../pages/ProductDetails"
+import NotFound from "../pages/NotFound"
+import UserDetails from "../pages/UserDetails"
+import UserAccount from "../pages/UserAccount"
+import UserLayout from "../components/UserLayout"
+
+const Navigator = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Products/>} />
+        <Route path="/product-details/:id" element={<ProductDetails/>} />
+
+        <Route path="*" element={<NotFound/>} />
+
+        <Route path="users" element={<UserLayout/>}>
+            <Route path="details" element={<UserDetails/>}/>
+            <Route path="account" element={<UserAccount />}/>
+        </Route>
+
+    </Routes>
+  )
+}
+
+export default Navigator
